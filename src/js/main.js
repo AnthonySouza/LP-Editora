a68dfad0bd9d6ddedf5ed0088d41f9c7a29d210a
AnthonySouza <gabrieldaniel1798@gmail.com>
2022-10-20 17:07:22 -0300

    Carrossel da LP adicionado

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-lancamentos').slick({
+      dots:true,
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000
+    });
+});
